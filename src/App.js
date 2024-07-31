872b07b2a2f3d3986f64200e9372080c2f3575cf
JM7160 <s2327@e-mirim.hs.kr>
2024-07-31 19:46:47 +0900

    Auto stash before merge of "main" and "origin/main"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>homelearn1</p>
+        <p>
+          homelearn
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
