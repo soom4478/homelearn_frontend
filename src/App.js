ba0d1269fcbacc5dd7c038b104a14c4b48041600
soomin <s2305@e-mirim.hs.kr>
2024-07-31 19:51:28 +0900

    test(main) : test2

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          homelearn
-        </p>
+        <p>homelearn2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
