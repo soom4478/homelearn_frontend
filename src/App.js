90b7bbd8c04f3ef08c5d5429b0151f85b207fd9a
JM7160 <s2327@e-mirim.hs.kr>
2024-08-31 17:24:50 +0900

    commit.JM

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PageMain from './MyPage/PageMain';
-import Profile from './MyPage/Profile';
-import MyPosts from './MyPage/MyPosts';
-import MyComments from './MyPage/MyComments';
-import MyReviews from './MyPage/MyReviews'
-import Settings from './MyPage/Settings';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TeamMain from './TeamInfo/TeamMain';
+import Player from './TeamInfo/Player';
+import Cheersong from './TeamInfo/Cheersong'; 
+import SongDetail from './TeamInfo/SongDetail'; 
+import Homeground from './TeamInfo/Homeground';
+import MapDetail from './TeamInfo/MapDetail';
 
-function App() {
+const App = () => {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<PageMain />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/myposts" element={<MyPosts />} />
-          <Route path="/mycomments" element={<MyComments />} />
-          <Route path="/myreviews" element={<MyReviews />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<TeamMain />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/cheersong" element={<Cheersong />} />
+        <Route path="/songdetail/:id" element={<SongDetail />} />
+        <Route path="/Homeground" element={<Homeground />} />
+        <Route path="/map-detail" element={<MapDetail />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
